304554ca61b71ccf8d1f661170ebeeb785190c94
m4xiz34 <maxisacroissant@gmail.com>
2026-04-29 18:40:09 +1000

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.ceil(45.490))
+print(math.floor(30.900))
+print(math.factorial(7))
+print(math.copysign(34,-2))
+print(math.sqrt(9))
+r=math.radians(70)
+print(r)
+print(math.gcd(39, 14))
+print(math.fabs(-69.89))
